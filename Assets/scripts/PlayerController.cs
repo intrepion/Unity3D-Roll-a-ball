7eff0bfc08121901483c7a88f914bf73dfde4ee3
Oliver Forral <intrepion@gmail.com>
2014-12-11 21:39:43 -0800

    Finished tutorial

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    private int count;
+
+    void Start ()
+    {
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rigidbody.AddForce (movement * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if ("PickUp" == other.gameObject.tag) {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText ();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (12 <= count) {
+            winText.text = "You Win!";
+        }
+    }
+}
---
+++ Assets/scripts/Rotater.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotater : MonoBehaviour
+{
+    void Update ()
+    {
+        Vector3 rotation = new Vector3 (15, 30, 45);
+
+        transform.Rotate (rotation * Time.deltaTime);
+    }
+}
